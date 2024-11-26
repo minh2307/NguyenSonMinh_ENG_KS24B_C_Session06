5c29857c164c754e24fdc2b4cf2eca67cd5308c4
minh2307 <minh23923@gmail.com>
2024-11-26 23:48:41 +0700

    session6_bai3

---
+++ session6_bai3.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int passwork;
+	do{
+		printf("nhap vao mat khau gom 2 so: ");
+		scanf("%d",&passwork);
+		
+}
+	while(passwork != 01);
+		
+	
+ 	
+	
+	
+	
+	
+return 0;
+}
